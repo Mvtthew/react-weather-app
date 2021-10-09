397bd9eeeabfa2309ad8809fc6e854c78f1077da
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-10-09 22:19:01 +0200

    :tada: Git ignore refresh

--- src/views/Home.view.tsx
+++ src/views/Home.view.tsx
@@ -1,29 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {DefaultLayout} from '../layouts/Detault.layout';
 import {WeatherAppCitySearch} from '../components/weather-app/WeatherAppCitySearch';
 import {WeatherAppCity} from '../components/weather-app/WeatherAppCity';
+import {useAppSelector} from '../hooks/useAppSelector';
 import {useOpenWeatherApi} from '../hooks/useOpenWeatherApi';
-import {WeatherForCityResponseType} from '../api/openWeather/types/WeatherForCityResponseType';
 
 export const HomeView: React.FC<{}> = (): JSX.Element => {
 
     const openWeatherApi = useOpenWeatherApi();
+    const {weatherData, cityName} = useAppSelector(state => state.weather);
 
-    const [weather, setWeather] = useState<WeatherForCityResponseType>();
+    const localizationDate = () => {
 
-    useEffect(() => {
-        openWeatherApi.getWeatherForCityName('Krakow').then(res => {
-            setWeather(res.data);
-        });
-    }, []);
+        return '15:00';
+    };
 
     return <>
         <DefaultLayout>
             <div className="d-flex justify-content-between p-4">
-                <WeatherAppCity cityName={'Kraków'} localizationDescription={'małopolskie, Polska'}/>
+                <WeatherAppCity cityName={cityName} localizationDescription={localizationDate()}/>
                 <WeatherAppCitySearch/>
             </div>
-            {weather && <img src={openWeatherApi.generateIconImageUrl(weather.weather[0].icon)}/>}
+            {weatherData && <img alt="Weather app - weather condition icon"
+			                     src={openWeatherApi.generateIconImageUrl(weatherData.weather[0].icon)}/>}
         </DefaultLayout>
     </>;
 };
